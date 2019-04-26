bc8b490f867e15c8aba80d39087700bc1a9a83d5
Andrew Davis <atd285@gmail.com>
2019-04-26 17:28:15 -0400

    editor and preview complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import marked from "marked";
+class App extends React.Component {
+  state = {
+    content: "# Type some markdown here!"
+  };
+  rawMarkup = () => {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false
+    });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let rawMarkup = marked(this.state.content, { sanitize: true });
+    return {
+      __html: rawMarkup
+    };
+  };
+  handleMarkDownChange = (event) => {
+    this.setState({ content: event.target.value });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className='header'>React Markdown Previewer</h1>
+        <div className='container'>
+          <div className='markdown-editor'>
+            <textarea
+              defaultValue={this.state.content}
+              onChange={this.handleMarkDownChange}
+            />
+          </div>
+          <div className='output' dangerouslySetInnerHTML={this.rawMarkup()} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
